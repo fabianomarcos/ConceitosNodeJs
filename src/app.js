e6c33dee9ed98df9cd7f43f08e0f170f4b89cd48
Fabiano Marcos <fabiano@vexter.com.br>
2020-08-19 21:51:48 -0300

    crud from the first GoStack test

--- src/app.js
+++ src/app.js
@@ -1,33 +1,91 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+const repositories = [
+  {
+    id: "18f386fb-86ec-43be-badc-49593592bafa",
+    title: "Umbr",
+    url: "https://github.com/Rocketseat/u",
+    techs: [
+      "React",
+      "ReactNative"
+    ],
+    likes: 0
+  }
+];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const projectIndex = repositories.findIndex(project => project.id === id);
+
+  if (projectIndex === -1) {
+    return response.status(400).json({ error: 'Projeto não encontrado.'});
+  }
+  const project = {
+    id, title, url, techs
+  };
+
+  repositories[projectIndex].title = project.title;
+  repositories[projectIndex].url = project.url;
+  repositories[projectIndex].techs = project.techs;
+
+  return response.json(repositories[projectIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const projectIndex = repositories.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Projeto não encontrado.'});
+  }
+
+  repositories.splice(projectIndex, 1);
+
+  return response.sendStatus(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return response.status(400).send();
+  }
+  repository.likes++;
+
+  return response.json(repository);
 });
 
 module.exports = app;
